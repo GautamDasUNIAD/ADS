a4ceb6a6ab37c7dcf8a4d13fc508f8abb075429f
GautamDasUNIAD <110016398+GautamDasUNIAD@users.noreply.github.com>
2023-07-26 15:32:59 +0930

    Check all possibilities

--- Referee.cpp
+++ Referee.cpp
@@ -9,12 +9,30 @@ Player * Referee::refGame(Player * player1, Player * player2){
     char p2move = player2->makeMove();
 
     // std::cout << p1move << p2move;
-    if (p1move == 'P'){
-        return player1;
-    } else if (p1move == 'R'){
-        return nullptr;
+    
+    if (p2move == 'R'){
+        if (p1move == 'P'){
+            return player1;
+        } else if (p1move == 'R'){
+            return nullptr;
+        } else {
+            return player2;
+        }
+    } else if (p2move == 'P'){
+        if (p1move == 'P'){
+            return nullptr;
+        } else if (p1move == 'R'){
+            return player2;
+        } else {
+            return player1;
+        }
     } else {
-        return player2;
+        if (p1move == 'P'){
+            return player2;
+        } else if (p1move == 'R'){
+            return player1;
+        } else {
+            return nullptr;
+        }
     }
-    return player1;
 }
